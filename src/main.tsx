85c419076eaa3e1b31c2afee733df7de601a6d23
Sayed Ahmed Husain <160347327+sahmedhusain@users.noreply.github.com>
2025-08-20 01:25:31 +0300

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { ApolloProvider } from '@apollo/client'
 import App from './App.tsx'
 import client from './graphql/client.ts'
 import './index.css'
+import { Analytics } from "@vercel/analytics/next"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
